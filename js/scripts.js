676317dfebeabfb7f2591bf053692354b97bb6ef
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-01 04:18:31 +0100

    ???update

--- js/scripts.js
+++ js/scripts.js
@@ -69,7 +69,71 @@ checkSymbolsElement.addEventListener("change", checkPassword)
 
 
 
+/* 
+const textPasswordElement = document.getElementById("textPassword")
+const numbersElement = document.getElementById("numbers")
+const rangeElement = document.getElementById("range")
+const vocabularyUpperElement = document.getElementById("vocabularyUpper")
+const vocabularyLowerElement = document.getElementById("vocabularyLower")
+const checkNumbersElement = document.getElementById("checkNumbers")
+const checkSymbolsElement = document.getElementById("checkSymbols")
+const generatePasswordElement = document.getElementById("generatePassword")
 
+const vocabularyUpper = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+const vocabularyLower = "abcdefghijklmnñopqrstuvwxyz"
+const checkNumbers = "0123456789"
+const checkSymbols = ",;.:-_<>`^[´¨{+*]çÇ}/*-+º!·$%&/()=¡|@#~€¬"
+
+
+const generatePassword = () => {
+    let textContent = ""
+    let contentCheck = ""
+    
+        for (let i = 0; i < rangeElement.value; i++)
+            {                
+                if (vocabularyUpperElement.checked)
+                    {
+                        contentCheck += vocabularyUpper
+                    }
+                if (vocabularyLowerElement.checked)
+                    {
+                        contentCheck += vocabularyLower
+                    }
+                if (checkNumbersElement.checked)
+                    {
+                        contentCheck += checkNumbers
+                    }
+                if (checkSymbolsElement.checked)
+                    {
+                        contentCheck += checkSymbols
+                    }
+                textContent += contentCheck.charAt(Math.floor(Math.random() * contentCheck.length))
+            }
+            textPasswordElement.value = textContent            
+}
+
+const changeTextLength = () => {
+    numbersElement.textContent = rangeElement.value
+}
+
+const checkPassword = () => {
+    
+    if (vocabularyUpperElement.checked || vocabularyLowerElement.checked || checkNumbersElement.checked || checkSymbolsElement.checked)
+        {
+            generatePasswordElement.disabled = false
+        }
+        else
+        {
+            generatePasswordElement.disabled = true
+        }
+}
+
+generatePasswordElement.addEventListener('click', generatePassword)
+rangeElement.addEventListener("input", changeTextLength)
+vocabularyUpperElement.addEventListener("change", checkPassword)
+vocabularyLowerElement.addEventListener("change", checkPassword)
+checkNumbersElement.addEventListener("change", checkPassword)
+checkSymbolsElement.addEventListener("change", checkPassword) */
 
 
 
